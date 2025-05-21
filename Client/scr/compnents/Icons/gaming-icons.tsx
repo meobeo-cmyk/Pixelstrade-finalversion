108843b05edb03ed5df042f5d682f993be3958e1
meobeo-cmyk <longid98s@gmail.com>
2025-05-21 22:30:45 +0700

    Add files via upload

---
+++ Client/scr/compnents/Icons/gaming-icons.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export const RobloxIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M5.164 0l13.67 13.67-13.67 13.67L5.164 0zm13.662 10.333L5.17 23.997l13.656-13.664z" />
+  </svg>
+);
+
+export const GamepadIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M8 6h8v2H8V6zm.5 12v-5h-1v5c0 .8-.7 1.5-1.5 1.5S4.5 18.8 4.5 18v-5c0-1.1.9-2 2-2H8c1.1 0 2 .9 2 2v5c0 .8-.7 1.5-1.5 1.5s-1.5-.7-1.5-1.5zm7 0v-5h-1v5c0 .8-.7 1.5-1.5 1.5s-1.5-.7-1.5-1.5v-5c0-1.1.9-2 2-2h2.5c1.1 0 2 .9 2 2v5c0 .8-.7 1.5-1.5 1.5s-1.5-.7-1.5-1.5z" />
+  </svg>
+);
+
+export const ChestIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M4 4h16v6h-2v-4H6v12h6v2H4V4zm12 8h-2v-2h2v2zm2 2v-2h2v2h-2zm-2 2h-2v-2h2v2zm0 2v2h-2v-2h2zm2-2h2v2h-2v-2zm0 4h-6v-2h2v-2h2v2h2v2z" />
+  </svg>
+);
+
+export const PokedexIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8zm0-12.5c-2.5 0-4.5 2-4.5 4.5s2 4.5 4.5 4.5 4.5-2 4.5-4.5-2-4.5-4.5-4.5zm0 5.5c-.5 0-1-.5-1-1s.5-1 1-1 1 .5 1 1-.5 1-1 1z" />
+  </svg>
+);
+
+export const SwordIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M7.05 13.406l3.534 3.536-1.413 1.414 1.415 1.415-1.414 1.414-2.122-2.122-2.121 2.122-1.414-1.414L5.636 18.35l-1.414-1.414 3.535-3.536 7.07-7.07 1.415-1.415 1.414 1.415-9.9 9.9 4.242 4.242 9.9-9.9L22.71 13l-1.414 1.414-9.9 9.9-7.07-7.072 1.414-1.414 1.414 1.414 7.071-7.07-2.828-2.83-7.07 7.072 1.414 1.414z" />
+  </svg>
+);
+
+export const CoinIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    className={className}
+    fill="currentColor"
+  >
+    <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8zm-1-15v3h2v-3h-2zm1 12c-2.2 0-4-1.8-4-4s1.8-4 4-4 4 1.8 4 4-1.8 4-4 4z" />
+  </svg>
+);
